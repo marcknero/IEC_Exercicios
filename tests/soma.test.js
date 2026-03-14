088e9cd5070befe54e8c6be2c9fbf108fca82194
Marcos <marck.nero@gmail.com>
2026-03-14 17:36:42 -0300

    fix: eslint config

--- tests/soma.test.js
+++ tests/soma.test.js
@@ -1,7 +1,7 @@
-function soma(a,b){
-	return a+b
+function soma(a, b) {
+    return a + b;
 }
 
-test("soma de 2+3 deve ser 5",()=>{
-expect(soma(2,3)).toBe(5);
+test('soma de 2+3 deve ser 5', () => {
+    expect(soma(2, 3)).toBe(5);
 });
