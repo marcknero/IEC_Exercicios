0d6e77c1f99f64da85b66ae490b75fdbcc3d6d5e
Marcos <marck.nero@gmail.com>
2026-06-12 18:58:07 -0300

    aula7

--- tests/enviaDados.test.js
+++ tests/enviaDados.test.js
@@ -1,23 +1,21 @@
-const api = { 
-    enviar: jest.fn(() => "Simulado!") 
-  };
-  
-  describe("Testando Mock em Arquivo Único", () => {
-  
+const api = {
+    enviar: jest.fn(() => 'Simulado!'),
+};
+
+describe('Testando Mock em Arquivo Único', () => {
     test("Deve retornar 'Simulado!' quando a função enviar for chamada", () => {
-      // Executa a função do mock
-      const resposta = api.enviar();
-      
-      // Validação 1: O retorno do texto está correto?
-      expect(resposta).toBe("Simulado!");
+        // Executa a função do mock
+        const resposta = api.enviar();
+
+        // Validação 1: O retorno do texto está correto?
+        expect(resposta).toBe('Simulado!');
     });
-  
-    test("Deve garantir que a função enviar foi de fato executada", () => {
-      // Executa novamente para o segundo teste
-      api.enviar();
-  
-      // Validação 2: O Jest confirma que a função foi chamada?
-      expect(api.enviar).toHaveBeenCalled();
+
+    test('Deve garantir que a função enviar foi de fato executada', () => {
+        // Executa novamente para o segundo teste
+        api.enviar();
+
+        // Validação 2: O Jest confirma que a função foi chamada?
+        expect(api.enviar).toHaveBeenCalled();
     });
-  
-  });
+});
--- tests/processamento.test.js
+++ tests/processamento.test.js
@@ -1,10 +1,10 @@
-function processarAlerta(valor){
-    if(valor>80) return 'Notificação enviada: Crítico';
-    if (valor>50) return 'Notificação enviada: Alto';
+function processarAlerta(valor) {
+    if (valor > 80) return 'Notificação enviada: Crítico';
+    if (valor > 50) return 'Notificação enviada: Alto';
     return 'moderado';
 }
 
-test('processamento completo de alerta critico', ()=>{
-    let resultado = processarAlerta(90);
-    expect(resultado).toBe("Notificação enviada: Crítico");
+test('processamento completo de alerta critico', () => {
+    const resultado = processarAlerta(90);
+    expect(resultado).toBe('Notificação enviada: Crítico');
 });
